af513605b499d776c33fe6e950d12ba31c2fc20c
Vincent Lefoulon <vincent.lefoulon@free.fr>
2018-01-07 11:17:57 +0100

    Load CSV data

--- viz.js
+++ viz.js
@@ -1,3 +1,11 @@
-(function() {
-    alert("test");
-})();
+$(document).ready(function() {
+    $.ajax({
+        type: "GET",
+        url: "https://raw.githubusercontent.com/Vayel/TEDTalksViz/master/data.csv",
+        dataType: "text",
+        success: function(data) {
+            console.log(data.substr(0, 20));
+            $("#loader").hide();
+        }
+    });
+});
