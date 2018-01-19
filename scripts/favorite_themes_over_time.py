3de045b2458307e243a1156610f6b31c6be60e7d
Vincent Lefoulon <vincent.lefoulon@free.fr>
2018-01-19 17:48:12 +0100

    Aggregate talks per theme

--- scripts/favorite_themes_over_time.py
+++ scripts/favorite_themes_over_time.py
@@ -6,16 +6,24 @@ if __name__ == "__main__":
 
     OUTPUT_PATH = os.path.join(DATA_DIR, extract_fname(__file__) + ".json")
 
-    data = defaultdict(list)
+    def mean(numbers):
+        return int(float(sum(numbers)) / max(len(numbers), 1))
+
+    views = defaultdict(lambda: defaultdict(list))
+    comments = defaultdict(lambda: defaultdict(list))
     for row in read_raw():
         date = get_date(row)
         for theme in tags_to_themes(get_tags(row)):
-            data[date].append({
-                "views": get_views(row),
-                "comments": get_comments(row),
-                "duration": get_duration(row),
-                "theme": theme,
-            })
+            views[date][theme].append(get_views(row))
+            comments[date][theme].append(get_comments(row))
 
+    data = {
+        date: [{
+            "views": mean(views[date][theme]),
+            "comments": mean(comments[date][theme]),
+            "theme": theme
+            } for theme in views[date]
+        ] for date in views
+    }
     data = dict_to_list(data, "date", "talks")
     write(OUTPUT_PATH, data)
