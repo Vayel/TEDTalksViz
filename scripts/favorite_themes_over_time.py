31448014d20fac7022aca3ae3ad06373519dabd9
Vincent Lefoulon <vincent.lefoulon@free.fr>
2018-01-19 12:20:24 +0100

    Add data for favorite themes

--- scripts/favorite_themes_over_time.py
+++ scripts/favorite_themes_over_time.py
@@ -1,25 +1,20 @@
 if __name__ == "__main__":
     import os
-    import csv
     import json
-    from functools import partial
     from collections import defaultdict
     from tools import *
 
     OUTPUT_PATH = os.path.join(DATA_DIR, extract_fname(__file__) + ".json")
 
-    with open(RAW_FILE) as f:
-        reader = csv.reader(f)
-        next(reader) # Skip header
-        data = defaultdict(list)
-        for row in reader:
-            date = get_date(row)
-            for theme in tags_to_themes(get_tags(row)):
-                data[date].append({
-                    "views": get_views(row),
-                    "comments": get_comments(row),
-                    "duration": get_duration(row),
-                    "theme": theme,
-                })
-        with open(OUTPUT_PATH, "w") as output:
-            json.dump(data, output, indent=2)
+    data = defaultdict(list)
+    for row in read_raw():
+        date = get_date(row)
+        for theme in tags_to_themes(get_tags(row)):
+            data[date].append({
+                "views": get_views(row),
+                "comments": get_comments(row),
+                "duration": get_duration(row),
+                "theme": theme,
+            })
+    with open(OUTPUT_PATH, "w") as output:
+        json.dump(data, output, indent=2)
--- scripts/thematic_distribution.py
+++ scripts/thematic_distribution.py
@@ -1,6 +1,5 @@
 if __name__ == "__main__":
     import os
-    import csv
     import json
     from functools import partial
     from collections import defaultdict
