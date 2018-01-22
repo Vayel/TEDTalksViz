955a170043c699090fab3ef5f111dfb6db870dfe
Maxime Deloche <maxime.deloche@ensimag.grenoble-inp.fr>
2018-01-22 19:01:53 +0100

    Add tags to themes json file

--- scripts/easy_tag.py
+++ scripts/easy_tag.py
@@ -9,18 +9,20 @@ data = {}
 i = 0
 step = 0
 for tag in list_tags(RAW_FILE):
-    step += 1
-    i += 1
-    themes = input(str(step) + ' - ' + tag + ": ").split(',')
-    data[tag] = themes
+	step += 1
+	i += 1
+	themes = input(str(step) + ' - ' + tag + ": ").split(',')
+	data[tag] = themes
 
-    # backup every X steps
-    if i == 5:
-        i = 0
-        with open('output_backup_'+str(step)+'.pkl', 'wb') as f:
-            pickle.dump(data, f)
-        
+	# backup every X steps
+	if i == 20:
+		print("Backup...")
+		i = 0
+		with open('output_backup_'+str(step)+'.pkl', 'wb') as f:
+			pickle.dump(data, f)
+		print("Done")
+
 
 with open('output.json', 'w') as f:
-    json.dump(data, f, indent=4)
+	json.dump(data, f, indent=4)
 
